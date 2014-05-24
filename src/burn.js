27f0d968078fc858774501f59cf24d659c4e5244
Antonino Caccamo <antonio@seltzlab.com>
2014-05-24 14:44:46 +0200

    a better burn

--- src/burn.js
+++ src/burn.js
@@ -15,32 +15,42 @@
     // default configuration
     Burn.defaultConfiguration = {
         min_radius: 10.0,
-        max_radius: 12.0
+        max_radius: 12.0,
+        radial_gradient_colors: ["#fff","#cc0000", '#3A2D23'],
+        radial_gradient_distribution: [0.5, 0.8, 1]
     };
 
     p.initialize = function(x, y) {
         
         this.ContainerInitialize();
 
-        var g = new createjs.Graphics().beginFill('#622').setStrokeStyle(0.1).beginStroke('#cc0000'); 
         this.radius = this.randomFloat(this.configuration.min_radius, this.configuration.max_radius);
+        var g = new createjs.Graphics().beginRadialGradientFill(this.configuration.radial_gradient_colors, this.configuration.radial_gradient_distribution, 0, 0, 1, 0, 0, this.radius).setStrokeStyle(0.1).beginStroke('#cc0000');
+        
         this.points = [];
         
-        //this.g.moveTo(this.radius, this.radius);
-        for (var angle = 1; angle < 360; angle += 1) {
+        //g.moveTo(0, 0);
+        var rad = 0,
+            arcrad = 0,
+            px = 0,
+            py = 0;
             
-            var rad = angle * (Math.PI / 180);
+        for (var angle = 0; angle <= 360; angle += 3) {
             
-            var px = this.radius * Math.cos(rad + this.noise(this.radius));
-            var py = this.radius * Math.sin(rad + this.noise(this.radius));
+            rad = angle * (Math.PI / 180);
             
-            g.lineTo(px, py);
+            px = this.radius * Math.cos(rad) + this.noise(angle);
+            py = this.radius * Math.sin(rad) + this.noise(angle);
+
+            arcrad = Math.random() * (4 - 2) + 2;
+            
+            g.arc(px, py, arcrad, rad + this.noise(angle), rad + this.noise(angle));
             
             this.points.push([px, py]);
         }
 
         this.s = new createjs.Shape(g);
-        this.s.alpha = 0.75;
+        //this.s.alpha = 0.8;
         
         this.addChild(this.s);
         
@@ -61,15 +71,14 @@
         createjs.Ticker.addEventListener("tick", tick);
         function tick() {
             
-            if (offset >= (totalIterations / 100 * 75)) {
+            if (offset >= (totalIterations / 100 * 80)) {
                 createjs.Tween.get(self.s).to({alpha: 0}, 500);
             }
              
             if (offset >= totalIterations || typeof self.points[offset] === 'undefined') {
                 createjs.Ticker.removeEventListener("tick", tick);
                 offset = 0;
-                console.log('removeit!');
-
+                
                 return;
             }
             
@@ -83,7 +92,7 @@
             var bounds = blurFilter.getBounds();
             s.cache(-50 + bounds.x, -50 + bounds.y, 100 + bounds.width, 100 + bounds.height);
             self.addChild(s);
-            offset = offset + 1 + parseInt((Math.random() * 12));
+            offset = offset + 1 + parseInt((Math.random() * 4));
             
             createjs.Tween.get(s).to({alpha: 0}, 1500);
         }
